d86774f83b4fb3e5927d0d0837146e590fac3eb4
Divya Krishna <30651931+DivyaKrishna97@users.noreply.github.com>
2017-09-03 11:41:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,76 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var article one = {
+    title: "article one | Divya Krishna";
+    heading: "article one";
+    date: " sep 2,2017";
+    content: " 
+    <p>
+         This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.
+    </p>
+    <p>
+         This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.
+    </p>
+    <p>
+         This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.
+    </p>"
+};
+
+
+function createTemplate(data) {
+    var title = data.title;
+    var date = data.date;
+    var heading = data.heading;
+    var content = data.content;
+  var htmlTemplate = "
+
+ <html>
+    <head>
+        <title>
+           ${title}
+        </title>
+        <meta name="viexport" content="width-device-width , initial-scale=1 "/>
+        <style>
+            .container{
+                   max-width: 800px;
+                   margin: 0 auto;
+                   color: grey;
+                   font-family: sans-serif;
+                   padding-left: 20px;
+                   padding-right: 20px;
+            }
+        </style>
+    </head> 
+     <body>
+          <div class="container">
+          <div>
+          <a href="/">Home</a>
+          </div>
+          <hr/>
+          <h3>
+             ${heading}
+          </h3>
+          <div>
+             ${date}
+          </div>
+          <div>
+             ${content}
+          </div>
+         </div>
+      </body>
+     </html>
+ ";
+ return htmlTemplate;
+ }
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one',function(req,res){
-res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+res.send( createTemplate(article one) );
 });
 app.get('/article-two',function(req,res){
 res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
