11c96ced615b42e62f338a22d13dacc10028e093
Divya Krishna <30651931+DivyaKrishna97@users.noreply.github.com>
2017-09-07 12:17:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,19 +1,26 @@
-function exec(){
-    var request = new XMLHttpRequest();
-    request.onreadystatechange = function() 
-    {
-          if (request.readyState === XMLHttpRequest.DONE) 
-          {
-              //Take Some Action
-              if (request.status === 200) 
-                { 
-                    var counter = request.responseText; 
-                    var span = document.getElementById('count');
-                    span.innerHTML = counter.toString();
-                } 
-          } 
-    }; 
-    request.open("GET", "http://lachireddyvani.imad.hasura-app.io/", true);
-    request.send(); 
-    
-}
+//counter code
+var button = document.getElementById("counter");
+button.oncLick = function() {
+  
+  //Create A Request To Counter Endpoint
+  var request = new XMLhttpRequest();
+  //Capture The Response And Store In HTML
+  request.onreadystatechange = function() {
+      if (request.readyState === XMLHttpRequest.DONE) 
+      {
+          //Take Some Action
+          if (httpRequest.status === 200) 
+            { 
+                var counter= request.responseText; 
+                var span = document.getElementById("count");
+                span.innerHTML = counter.toString();
+            } 
+          else { alert('There was a problem with the request.'); }
+      
+      } 
+      
+  };
+  //Make A Request
+  request.open('GET', 'http://kogam22.imad.hasura-app.io/counter', true);
+  request.send(null);
+};
