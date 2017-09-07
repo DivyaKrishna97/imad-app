e2bccb3c51bcea0e7ea90db4cfb293d21d0fee86
Divya Krishna <30651931+DivyaKrishna97@users.noreply.github.com>
2017-09-07 17:24:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,3 +24,23 @@ button.oncLick = function() {
   request.open('GET', 'http://lachireddyvani.imad.hasura-app.io/counter', true);
   request.send(null);
 };
+
+//submit name
+var nameinput = document.getElementById("name");
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function() {
+    
+    //make a request to the server and send the name
+    
+    //capture a list of names and render it to the list
+    
+    var names = ['name1', 'name2', 'name3', 'name4'];
+    var list = '';
+    for(var i=0; i<names.lenght; i++) {
+        list +='<li>' + names[i] + '<li>';
+    }
+    var ul = document.getElementById('namelist');
+    ul.innerHTML = list;
+};
+ 
