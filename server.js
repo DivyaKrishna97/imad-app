e07a4a5c300c1f13960cedce17a02e2fe40b50f9
Divya Krishna <30651931+DivyaKrishna97@users.noreply.github.com>
2017-09-07 11:13:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -78,9 +78,10 @@ res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 var counter = 0;
-app.get('/counter',function (req,res) {
-    counter = counter + 1;
-    res.send(counter.tostring());
+app.get('/counter', function ( req, res) {
+counter = counter + 1;
+res.send(counter.toString());
+
 });
 
 app.get('/:articleName', function (req, res) {
