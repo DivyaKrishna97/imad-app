a4457bc17c55c7f441bcedeab5303c36755081be
Divya Krishna <30651931+DivyaKrishna97@users.noreply.github.com>
2017-09-07 12:29:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+
 var app = express();
 app.use(morgan('combined'));
-var articles = {
-
-'article one': {
-title: 'article One | Divya Krishna',
-heading: 'article one',
-date: 'sep 2, 2017',
-    content:`
+var articles = 
+{
+     'article-one'   : {
+    title: 'Article One | Divya Krishna',
+    heading: 'Article One',
+    date: "Aug 5. 2017",
+    content: `
         <p>
             This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.
         </p>
@@ -41,69 +42,84 @@ date: 'sep 12, 2017',
                 </p> `
 },
 };
-function createTemplate(data) {
+function createTemplate (data) {
+
     var title = data.title;
     var date = data.date;
     var heading = data.heading;
     var content = data.content;
-    
-  var htmlTemplate=`
+    var htmlTemplate = `
+
+<!DOCTYPE html>
 <html>
-<head>
-<title>
-${title}
-</title>
-<meta name="viewport" contents="width=device-width, initial-scale=1" />
-<link href="/ui/style.css" rel="stylesheet" />
-</head>
-<body>
-<div class="container">
-<div>
-<a href="/">Home</a>
-</div>
-<hr/>
-<h3>${heading}</h3>
-<div>${date}</div>
-<div>
-${content}
-</div>
-</div>
-</body>
-</html>`;
-return htmlTemplate;
+    <head>
+        <title>
+            ${title}
+        </title>
+        <meta name="viewport" consent="width=device-width, initial-scale=1" />
+        <!--Above Line Is Used For Adjusting Width Of Screen According To Display-->
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    
+    
+    <body>
+        <div class="container">
+            <div>
+                <a href='/'>Home</a>
+            </div>
+            <hr/>
+            <h3>
+                ${heading}
+            </h3>
+            <div>
+                ${date}
+            </div>
+            <div>
+               ${content}
+            </div>
+        </div>
+    </body>
+
+</html>
+                        `;
+    return htmlTemplate;
+
+
 }
 
-app.get('/', function (req, res) {
-res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+app.get('/favicon.ico', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'favicon.ico'));
 });
 
-var counter = 0;
+var counter=0;
 app.get('/counter', function (req, res) {
-      counter = counter + 1;
-      res.send(counter.toString());
+   counter = counter + 1;
+   res.send(counter.toString());
+});
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/:articleName', function (req, res) {
-var articleName = req.params.articleName;
-res.send(createTemplate(articles[articleName]));
+    //articleName == article-one
+    //articles[articleName] == {} content object for article
+    var articleName = req.params.articleName;
+	res.send(createTemplate(articles[articleName]));
+});
+
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
 app.get('/ui/style.css', function (req, res) {
-res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-app.get('/ui/main.js', function (req, res) {
-res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
-res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-app.get('/ui/favicon.ico', function (req, res) {
-
-  res.sendFile(path.join(__dirname, 'ui', 'favicon.ico'));
-
-});
 
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
